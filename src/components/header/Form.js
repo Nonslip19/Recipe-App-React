9232d9bf9972eb2c704bcc7ad9aebbdbd59b6404
Mehmet <mkaymaz93@gmail.com>
2022-02-13 00:34:22 +0300

    home

--- src/components/header/Form.js
+++ src/components/header/Form.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import { Button, FoodInput, FormContainer, Select } from './HeaderStyle'
 
-const Form = () => {
+const Form = ({setQuery, query, getData, mealTypes, setMeal, meal}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getData();
+  }
+
   return (
     <div>
-        <FormContainer>
+        <FormContainer onSubmit={handleSubmit}>
             <FoodInput type="text"
-            placeholder="Search"/>
-            <Button> Search </Button>
-            <Select></Select>
+            placeholder="Search"
+            onChange={(e)=>setQuery(e.target.value)}  
+            />
+            <Button onClick={getData}> Search </Button>
+            <Select
+            name='mealTypes'
+            id='mealTypes'
+            onChange={(e)=>setMeal(e.target.value)}>
+                {mealTypes?.map((item, index)=>(
+                  <option key={index} value={item.toLowerCase()}>
+                      {item}
+                  </option>
+                ))}
+            </Select>
         </FormContainer>
     </div>
   )
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,26 +1,60 @@
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import Header from '../../components/header/Header';
+import { HomeImg, ImgDiv, MainContainer } from './HomeStyle';
+import RecipeCardComp from './RecipeCardComp';
+import homeSvg from '../../assets/home.svg';
 
 const mealTypes = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"];
-// const API_ID="f6a641e4";
-// const APP_KEY = "f3436c27a6041af35bf7f248b0ccc906";
+const APP_ID="f6a641e4";
+const APP_KEY = "f3436c27a6041af35bf7f248b0ccc906";
 
 const Home = () => {
-  const [query, setQuery] = useState("pizza");
+  const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState();
   const [meal, setMeal] = useState(mealTypes[0].toLowerCase());
 
-  const url = `https://api.edamam.com/api/recipes/v2/f6a641e4?type=public&app_id=f6a641e4&app_key=f3436c27a6041af35bf7f248b0ccc906&field=${query}`;
+  const url=`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
 
   const getData = async() => {
     if(query !== ""){
       const result = await axios.get(url);
-      console.log(result);
+      console.log(result.data.hits);
+      if(!result.data.more){
+        console.log("no food a this name");
+      }
+      setRecipes(result.data.hits)
+      setQuery("")
+    }else{
+      console.log("Please fill the form");
     }
-  }
+  };
+
+  useEffect(() => {
+    getData();
+  })
 
   return (
-    <div>Home</div>
+    <div>
+      <Header
+      setQuery={setQuery}
+      query={query}
+      getData={getData}
+      mealTypes={mealTypes}
+      setMeal={setMeal}
+      meal={meal}
+     />
+      {recipes?(
+      <MainContainer>
+        {recipes.map((recipe, index)=>(
+          <RecipeCardComp key={index} recipe={recipe.recipe}/>
+          ))}
+      </MainContainer> ) : 
+      <ImgDiv>
+        <HomeImg src={homeSvg}/>
+      </ImgDiv>
+      }
+    </div>    
   )
 }
 
